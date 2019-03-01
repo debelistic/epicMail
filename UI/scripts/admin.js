a75e270afff4d7dff42155b38e3091f516afd7d9
Victor Awotidebe <victorawotidebe@gmail.com>
2019-03-01 14:19:48 +0100

    [Feature #164193845]Add Admin pages

---
+++ UI/scripts/admin.js
@@ -0,0 +1,42 @@
+const ul = document.querySelector('#groupid');
+const addGroupForm = document.forms.addgroupform;
+const groupList = document.querySelector('#groupid');
+const groupName = document.querySelector('#groupname');
+
+/**
+ * delete group
+ */
+groupList.addEventListener('click', (event) => {
+    if(event.target.className == 'delete'){
+        const li = event.target.parentElement;
+        groupList.removeChild(li);
+    }
+})
+
+
+/**
+ * add group
+ */
+ addGroupForm.addEventListener('submit', (event) => {
+
+    //prevent form form reloading
+     event.preventDefault();
+     
+     //create new group
+     const newgroupname = groupName.value;
+     if(newgroupname === "") {
+         return
+     } else{
+        const li = document.createElement('li');
+        const remove = document.createElement('span');
+        li.innerText = newgroupname;
+        remove.className = 'delete';
+        remove.innerText = 'Delete';
+     
+        li.appendChild(remove);
+        groupList.appendChild(li);
+        addGroupForm.reset();
+    }     
+ })
+
+
