8fa4fdb282e605d06fc60e64aa46317c7b8a87ea
Victor Awotidebe <victorawotidebe@gmail.com>
2019-03-02 10:48:55 +0100

    [Chore #164348763]Fix Eslint Errors

--- UI/scripts/admin.js
+++ UI/scripts/admin.js
@@ -7,17 +7,17 @@ const groupName = document.querySelector('#groupname');
  * delete group
  */
 groupList.addEventListener('click', (event) => {
-    if(event.target.className == 'delete'){
-        const li = event.target.parentElement;
-        groupList.removeChild(li);
-    }
-})
+  if (event.target.className == 'delete') {
+    const li = event.target.parentElement;
+    groupList.removeChild(li);
+  }
+});
 
 
 /**
  * add group
  */
- addGroupForm.addEventListener('submit', (event) => {
+addGroupForm.addEventListener('submit', (event) => {
 
     //prevent form form reloading
      event.preventDefault();
@@ -26,7 +26,7 @@ groupList.addEventListener('click', (event) => {
      const newgroupname = groupName.value;
      if(newgroupname === "") {
          return
-     } else{
+     } 
         const li = document.createElement('li');
         const remove = document.createElement('span');
         li.innerText = newgroupname;
@@ -36,7 +36,5 @@ groupList.addEventListener('click', (event) => {
         li.appendChild(remove);
         groupList.appendChild(li);
         addGroupForm.reset();
-    }     
- })
-
-
+         
+ });
--- UI/scripts/main.js
+++ UI/scripts/main.js
@@ -2,7 +2,5 @@ const navbutton = document.querySelector('i#navbutton');
 const navmenu = document.querySelector('nav ul');
 
 navbutton.addEventListener('click', () => {
-    navmenu.classList.toggle('open');
-
-})
-
+  navmenu.classList.toggle('open');
+});
