9ac53e78aacfbd6d4e8b04b093e9114e9f1b4491
Victor Awotidebe <victorawotidebe@gmail.com>
2019-03-03 09:28:47 +0100

    [Feature #164351675]Add Model for Mail

---
+++ server/jsobjectsdb/models/Mail.js
@@ -0,0 +1,69 @@
+/**
+ * randomId function is from the discussion on this stackoverflow link
+ * https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+ * the function is modified here to generate 15 length character id for our data base
+ */
+const randomId = () => {
+  let id = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let index = 0; index < 15; index += 1) {
+    id += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return id;
+};
+
+class Mail {
+  constructor() {
+    this.inbox = [];
+    this.sent = [];
+    this.drafts = [];
+  }
+
+  create(mail) {
+    const newMail = {
+      id: randomId(),
+      createdOn: Date(),
+      subject: mail.subject,
+      message: mail.message,
+      parentMessageId: mail.parentMessageId,
+      status: Boolean,
+    };
+
+    if (newMail.status === true) {
+      this.sent.push(newMail);
+      return { message: 'Mail Sent' };
+    }
+    this.drafts.push(newMail);
+    return { message: 'Saved to darfts' };
+  }
+
+  getInbox() {
+    return this.inbox;
+  }
+
+  getAInbox(id) {
+    const aInbox = this.inbox.find(ainbox => ainbox.id === id);
+    return aInbox;
+  }
+
+  getSent() {
+    return this.sent;
+  }
+
+  getASent(id) {
+    const aSent = this.sent.find(asent => asent.id === id);
+    return aSent;
+  }
+
+  getDrafts() {
+    return this.drafts;
+  }
+
+  getADraft(id) {
+    const aDraft = this.drafts.find(adraft => adraft.id === id);
+    return aDraft;
+  }
+}
+
+export default Mail;
