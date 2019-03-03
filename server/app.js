3fba699a314bbaa24f2c92971209ecff2a351714
Victor Awotidebe <victorawotidebe@gmail.com>
2019-03-03 03:35:42 +0100

    [Feature #164351675]Set app server to test db

--- server/app.js
+++ server/app.js
@@ -1,3 +1,13 @@
-import env from './config';
+import express from 'express';
 
-console.log(env.port, env.dev);
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('welcome');
+});
+
+const port = 5000 || process.env.PORT;
+
+app.listen(port);
