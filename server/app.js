0c9f92fb78320b20e8e65edf17ea884be3af2ac5
Victor Awotidebe <victorawotidebe@gmail.com>
2019-03-08 21:56:36 +0100

    deploy : fix server error
    [Delivers #164351273]

--- server/app.js
+++ server/app.js
@@ -8,6 +8,9 @@ dotenv.config();
 
 app.use(express.json());
 app.use('/api/v1/users', userRoutes);
+app.get('/', (req, res) => {
+  res.status(200).send('WELCOME TO EPICMAIL SERVICE');
+});
 
 const port = process.env.PORT;
 app.listen(port);
