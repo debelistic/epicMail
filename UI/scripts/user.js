40c92cb5ddbf0ca767d7352699ac32019fc96e4a
Victor Awotidebe <victorawotidebe@gmail.com>
2019-04-01 18:57:21 +0100

    feat(user) - create mobile responsive dashboards

--- UI/scripts/user.js
+++ UI/scripts/user.js
@@ -3,14 +3,22 @@ const mailform = document.querySelector('form#mailform');
 const userimage = document.querySelector('#profiledropdown');
 const usernav = document.querySelector('nav.user-options');
 const largecompose = document.querySelector('#large-compose');
+const menuicon = document.querySelector('#menu-icon');
+const usermenu = document.querySelector('.usermenu');
 
 
 largecompose.addEventListener('click', () => {
+  usermenu.classList.remove('open');
   mailform.classList.toggle('open');
 });
 
 userimage.addEventListener('click', () => {
   usernav.classList.toggle('open');
+});
+
+menuicon.addEventListener('click', () => {
+  mailform.classList.remove('open');
+  usermenu.classList.toggle('open');
 });
 
 document.addEventListener('click', () => {
