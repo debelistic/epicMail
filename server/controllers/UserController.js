314cee2392a2694bb38ea373f9b7553de27c64bc
Victor Awotidebe <victorawotidebe@gmail.com>
2019-04-05 01:57:55 +0100

    Bug(validation) - fix validation bugs
    - fix user validation bugs
    - fix groups validation bugs
    [Bug #165137321]

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -4,8 +4,12 @@ import Helper from '../middleware/Helper';
 
 const UserController = {
   /**
-     * create user
-     */
+   * Create user controller
+   * @param {object} req
+   * @param {object} res
+   * @param {object} next
+   * @returns {object} user
+   */
   async createUser(req, res, next) {
     try {
       const securityKey = req.body.securityKey.toLowerCase();
@@ -38,31 +42,22 @@ const UserController = {
         }],
       });
     } catch (error) {
-      if (error.routine === '_bt_check_unique') {
-        return res.status(400).send({
-          message: 'User alredy exist',
-        });
-      }
       return next(error);
     }
   },
 
   /**
-   * user login
+   * Login controller
+   * @param {object} req
+   * @param {object} res
+   * @param {object} next
+   * @returns {object} user
    */
   async login(req, res, next) {
     try {
       const loginQuery = 'SELECT * FROM users WHERE email = $1';
-      const userEmail = await `${req.body.email.toLowerCase()}@epicmail.com`;
+      const userEmail = await `${req.body.email.toLowerCase()}`;
       const { rows } = await db.query(loginQuery, [userEmail]);
-      if (!rows[0]) {
-        return res.status(400).send({
-          message: 'User not registerd',
-        });
-      }
-      if (!Helper.comparePassword(req.body.password, rows[0].password)) {
-        return res.status(400).send({ message: 'Invalid password' });
-      }
       const token = Helper.generateToken(rows[0].email);
       return res.status(200).send({
         status: 200,
@@ -81,36 +76,6 @@ const UserController = {
    * @param {object} req
    * @param {object} res
    */
-  async resetPassword(req, res, next) {
-    try {
-      const getUserSecurityQuestion = 'SELECT * FROM users WHERE $1 = email';
-      const values = [
-        req.body.email,
-      ];
-      const { rows } = await db.query(getUserSecurityQuestion, values);
-      if (!rows) {
-        return res.status(400).send({ message: 'User not found' });
-      }
-
-      const security = rows[0].securitykey;
-      const answer = req.body.securityKey.toLocaleLowerCase();
-      if (!Helper.comparePassword(answer, security)) {
-        return res.status(400).send({ message: 'Your answer is incorrect' });
-      }
-
-      const newPassword = Helper.hashPassword(req.body.password);
-      const updateUserPassword = 'UPDATE users SET password=$1 WHERE $2 = email RETURNING *';
-      await db.query(updateUserPassword, [newPassword, req.body.email]);
-      return res.status(201).send({
-        status: 201,
-        data: [{
-          message: 'Your password has been successfully changed',
-        }],
-      });
-    } catch (error) {
-      return next(error);
-    }
-  },
 };
 
 export default UserController;
